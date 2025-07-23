f46fa30d25c4420bab63bb9cad0fe543435b7f41
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-07-23 20:18:52 +0300

    Add NestJS library setup with base configuration and dependencies

--- apps/jobber-auth/src/app/app.module.ts
+++ apps/jobber-auth/src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: true
+  })],
 })
 export class AppModule {}
--- apps/jobber-auth/src/app/prisma/prisma.service.ts
+++ apps/jobber-auth/src/app/prisma/prisma.service.ts
@@ -5,5 +5,6 @@ import { PrismaClient } from '@prisma-clients/jobber-auth';
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect()
+    console.log('Prisma connected');
   }
 }
