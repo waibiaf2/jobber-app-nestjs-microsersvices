cda388941bf400ab6f70e7425566000c800b2228
Waibi Andrew Franklin <waibiaf2@gmail.com>
2025-08-01 20:13:46 +0300

    Update tests in Auth module

--- apps/jobber-auth/src/app/auth/auth.resolver.spec.ts
+++ apps/jobber-auth/src/app/auth/auth.resolver.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthResolver } from './auth.resolver';
+import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthResolver],
+      providers: [
+        AuthResolver,
+        AuthService,
+        {
+          provide: AuthService,
+          useValue: {
+            login: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
--- apps/jobber-auth/src/app/auth/auth.service.spec.ts
+++ apps/jobber-auth/src/app/auth/auth.service.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        { provide: AuthService, useValue: { login: jest.fn() } }, // Mock AuthService methods if needed
+        { provide: ConfigService, useValue: {} },
+        { provide: JwtService, useValue: {} }, // Mock JwtService if needed
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
